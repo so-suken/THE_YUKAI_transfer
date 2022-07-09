186e4910d4eda5e364e2491f979d0bb171a02d0d
Glitch (pumped-chopper) <none>
2022-07-09 13:07:30 +0000

    📙🌚 Checkpoint ./main.js:46645475/3317 ./package.json:46645475/1807

--- main.js
+++ main.js
@@ -1,33 +1,118 @@
-// Response for Uptime Robot
-const http = require('http');
-http.createServer(function(request, response)
-{
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Discord bot is active now \n');
+// Response for GAS
+const http = require("http");
+const querystring = require('querystring');
+
+http.createServer(function(req, res){
+ if (req.method == 'POST'){
+   var data = "";
+   req.on('data', function(chunk){
+     data += chunk;
+   });
+   req.on('end', function(){
+     if(!data){
+        console.log("No post data");
+        res.end();
+        return;
+     }
+     var dataObject = querystring.parse(data);
+     console.log("post:" + dataObject.type);
+     if(dataObject.type == "wake"){
+       console.log("Woke up in post");
+       res.end();
+       return;
+     }
+     res.end();
+   });
+ }
+ else if (req.method == 'GET'){
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Discord Bot is active now\n');
+ }
 }).listen(3000);
 
 // Discord bot implements
-const discord = require('discord.js');
+const discord = require("discord.js");
 const client = new discord.Client();
 
-client.on('ready', message =>
-{
-	.log('bot is ready!');
+client.on("ready", message => {
+  // botのステータス表示
+  client.user.setPresence({ game: { name: "with discord.js" } });
+  console.log("bot is ready!");
 });
 
-client.on('message', message =>
-{
-	if(message.isMemberMentioned(client.user))
-	{
-		message.reply( '呼びましたか？' );
-		return;
-	}
+client.on("message", message => {
+  if (message.author.bot) {
+    return;
+  }
+  // DMには応答しない
+  if (message.channel.type == "dm") {
+    return;
+  }
+
+  var msg = message;
+
+  // botへのリプライは無視
+  if (msg.mentions.has(client.user)) {
+    return;
+  } else {
+    //GASにメッセージを送信
+    sendGAS(msg);
+    return;
+  }
+
+  function sendGAS(msg) {
+    var jsonData = {
+      events: [
+        {
+          type: "discord",
+          name: msg.author.username,
+          message: msg.content
+        }
+      ]
+    };
+    //GAS URLに送る
+    console.log(msg.author.username);
+    console.log(msg.content);
+    post(process.env.GAS_URL, jsonData);
+  }
+
+  function post(url, data) {
+    //requestモジュールを使う
+    var request = require("request");
+    var options = {
+      uri: url,
+      headers: { "Content-type": "application/json" },
+      json: data,
+      followAllRedirects: true
+    };
+    // postする
+    request.post(options, function(error, response, body) {
+      if (error != null) {
+        msg.reply("更新に失敗しました");
+        console.log("更新に失敗しました");
+        return;
+      }
+
+      var userid = response.body.userid;
+      var channelid = response.body.channelid;
+      var message = response.body.message;
+      if (
+        userid != undefined &&
+        channelid != undefined &&
+        message != undefined
+      ) {
+        var channel = client.channels.get(channelid);
+        if (channel != null) {
+          channel.send(message);
+        }
+      }
+    });
+  }
 });
 
-if(process.env.DISCORD_BOT_TOKEN == undefined)
-{
-	console.log('please set ENV: DISCORD_BOT_TOKEN');
-	process.exit(0);
+if (process.env.DISCORD_BOT_TOKEN == undefined) {
+  console.log("please set ENV: DISCORD_BOT_TOKEN");
+  process.exit(0);
 }
 
-client.login( process.env.DISCORD_BOT_TOKEN );
+client.login(process.env.DISCORD_BOT_TOKEN);
